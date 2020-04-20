d8272d9c8189aa04f627425598e9f07ba403e8f2
Amanda Polotow <amandapolotow@gmail.com>
2020-04-20 18:54:36 -0300

    Finished chellenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialValueIncome = 0;
+    const initialValueOutcome = 0;
+
+    const reducer = (
+      accumulator: number,
+      currentValue: Transaction,
+    ): number => {
+      return accumulator + currentValue.value;
+    };
+
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce(reducer, initialValueIncome);
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce(reducer, initialValueOutcome);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
